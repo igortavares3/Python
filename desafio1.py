b1dd461c9b959cbf0d3ebf99e934c4fd301bb634
Igor Tavares <igorviniciusliradasilva7@gmail.com>
2025-07-08 18:43:56 -0300

    desafio1

---
+++ desafio1.py
@@ -0,0 +1,3 @@
+nome = input ('Qual é o seu nome? ')
+
+print ('Ola'  + nome, '! Prazer em te conhecer')
---
+++ ex001.py
@@ -0,0 +1,7 @@
+nome = input ('Qual é o seu nome? ')
+idade = input ('Qual a sua idade? ')
+peso = input ('Qual é o seu peso? ')
+
+print ('Ola Igor ! Prazer em te conhecer' , nome)
+print ('Minha idade é: 19 Anos' , idade )
+       
