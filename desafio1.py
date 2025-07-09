8981d2f728d087cd18d5ca7feb42431a805f63cf
Igor Tavares <igorviniciusliradasilva7@gmail.com>
2025-07-08 22:56:02 -0300

    desafio 01 inpunt

--- desafio1.py
+++ desafio1.py
@@ -1,3 +1,6 @@
 nome = input ('Qual é o seu nome? ')
+idade = input ('Qual a sua idade? ')
 
-print ('Ola'  + nome, '! Prazer em te conhecer')
+print ('Ola '  + nome +'! Prazer em te conhecer') 
+print ('Minha idade é:' , idade) 
+
